183ffe7cbc100dd2b3fcbe2e6754b38f3bd7725f
seongmin <seongm22n@gmail.com>
2023-09-16 12:00:50 +0900

    refactor : skill 파트 추가 및 수정

--- main.js
+++ main.js
@@ -4,61 +4,57 @@
 const navbar = document.querySelector('#header');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight){
-        navbar.classList.add('navbar--dark');
-    }else {
-        navbar.classList.remove('navbar--dark');
-        
-    }
+	if (window.scrollY > navbarHeight) {
+		navbar.classList.add('navbar--dark');
+	} else {
+		navbar.classList.remove('navbar--dark');
+	}
 });
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if(link == null) {
-        return;
-    }
-    navbarMenu.classList.remove('open');
-    scrollIntoView(link);
+	const target = event.target;
+	const link = target.dataset.link;
+	if (link == null) {
+		return;
+	}
+	navbarMenu.classList.remove('open');
+	scrollIntoView(link);
 });
 
 //Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
-    navbarMenu.classList.toggle('open');
+	navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
-    scrollIntoView('#contact');
+	scrollIntoView('#contact');
 });
-
 
 //Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    home.style.opacity = 1 - window.scrollY / homeHeight;
+	home.style.opacity = 1 - window.scrollY / homeHeight;
 });
-
 
 //Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
-    if(window.scrollY > homeHeight / 2){
-        arrowUp.classList.add('visible');
-    } else {
-        arrowUp.classList.remove('visible');
-    }
+	if (window.scrollY > homeHeight / 2) {
+		arrowUp.classList.add('visible');
+	} else {
+		arrowUp.classList.remove('visible');
+	}
 });
-
 
 //Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
-    scrollIntoView('#home');
+	scrollIntoView('#home');
 });
 
 //Projects
@@ -66,56 +62,96 @@ const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 workBtnContainer.addEventListener('click', (e) => {
-    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-    if(filter == null) {
-        return;
-    }
-    //Remove selection from the previous item and select the new one
-    const active = document.querySelector('.category__btn.selected')
-    active.classList.remove('selected');
-    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('selected');
+	const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+	if (filter == null) {
+		return;
+	}
+	//Remove selection from the previous item and select the new one
+	const active = document.querySelector('.category__btn.selected');
+	active.classList.remove('selected');
+	const target =
+		e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+	target.classList.add('selected');
 
-    projectContainer.classList.add('anim-out');
-    setTimeout(() => {
-        projects.forEach((project) => {
-            console.log(project.dataset.type);
-            if(filter === '*' || filter === project.dataset.type){
-                project.classList.remove('invisible');
-            }  else {
-                project.classList.add('invisible');
-            } 
-        });
-        projectContainer.classList.remove('anim-out');
-    },300);
+	projectContainer.classList.add('anim-out');
+	setTimeout(() => {
+		projects.forEach((project) => {
+			// console.log(project.dataset.type);
+			if (filter === '*' || filter === project.dataset.type) {
+				project.classList.remove('invisible');
+			} else {
+				project.classList.add('invisible');
+			}
+		});
+		projectContainer.classList.remove('anim-out');
+	}, 300);
 });
 
+/*==================== Skills  ====================*/
+const skillBtnContainer = document.querySelector('.skill__categories');
+const skillContainer = document.querySelector('.skill__stacks');
+const skills = document.querySelectorAll('.skill');
+
+// 초기 렌더링 시 "Front End" 데이터 타입에 해당하는 스킬 아이템만 표시
+skills.forEach((skill) => {
+	if (skill.dataset.type === 'frontend') {
+		skill.classList.remove('invisible');
+	} else {
+		skill.classList.add('invisible');
+	}
+});
+
+skillBtnContainer.addEventListener('click', (e) => {
+	const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+	if (filter == null) {
+		return;
+	}
+	//Remove selection from the previous item and select the new one
+	const active = document.querySelector('.skill__btn.selected');
+	active.classList.remove('selected');
+	const target =
+		e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+	target.classList.add('selected');
+
+	skillContainer.classList.add('anim-out');
+	setTimeout(() => {
+		skills.forEach((skill) => {
+			if (filter === '*' || filter === skill.dataset.type) {
+				skill.classList.remove('invisible');
+			} else {
+				skill.classList.add('invisible');
+			}
+		});
+		skillContainer.classList.remove('anim-out');
+	}, 300);
+});
+
+/*==================== SCROLL ====================*/
 
 function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+	const scrollTo = document.querySelector(selector);
+	scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
-
 
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
-    modalBtns = document.querySelectorAll('.services__button'),
-    modalCloses = document.querySelectorAll('.services__modal-close')
+	modalBtns = document.querySelectorAll('.services__button'),
+	modalCloses = document.querySelectorAll('.services__modal-close');
 
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
+let modal = function (modalClick) {
+	modalViews[modalClick].classList.add('active-modal');
+};
 
 modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () =>{
-        modal(i)
-    })
-})
+	modalBtn.addEventListener('click', () => {
+		modal(i);
+	});
+});
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click', () => {
-        modalViews.forEach((modalView) =>{
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach((modalView) => {
+			modalView.classList.remove('active-modal');
+		});
+	});
+});
